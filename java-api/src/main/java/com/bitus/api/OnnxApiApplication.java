9e36e7378c582fd1aec79aa5bf6a2200f285488e
westing88 <127288518+westing88@users.noreply.github.com>
2025-06-10 16:23:00 -0700

    Create OnnxApiApplication.java

---
+++ java-api/src/main/java/com/bitus/api/OnnxApiApplication.java
@@ -0,0 +1,11 @@
+package com.bitus.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnnxApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OnnxApiApplication.class, args);
+    }
+}
